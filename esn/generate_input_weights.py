743cbdc28da775c1db96f41c302e44679ec49f4d
defneozan <d.ozan@imperial.ac.uk>
2025-11-10 16:20:47 +0000

    Updated tutorial and added dense input weights option for the input bias

--- esn/generate_input_weights.py
+++ esn/generate_input_weights.py
@@ -5,10 +5,10 @@ from scipy.sparse import lil_matrix
 
 def sparse_random(W_in_shape, W_in_seeds):
     """Create the input weights matrix
-    Inputs are not connected, except for the parameters
+    Inputs are not connected and the weights are randomly placed one per row.
 
     Args:
-        W_in_shape: N_reservoir x (N_inputs + N_input_bias + N_param_dim)
+        W_in_shape: N_reservoir x (N_inputs + N_input_bias)
         seeds: a list of seeds for the random generators;
             one for the column index, one for the uniform sampling
     Returns:
@@ -35,6 +35,41 @@ def sparse_random(W_in_shape, W_in_seeds):
 
     return W_in
 
+def sparse_random_dense_input_bias(W_in_shape, W_in_seeds, input_bias_len):
+    """Create the input weights matrix.
+    Inputs are not connected and the weights are randomly placed one per row
+    However, the input bias is densely connected to ALL reservoir state variables
+
+    Args:
+        W_in_shape: N_reservoir x (N_inputs + N_input_bias)
+        W_in_seeds: a list of seeds for the random generators;
+            one for the column index, one for the uniform sampling
+    Returns:
+        W_in: sparse matrix containing the input weights
+    """
+    # initialize W_in with zeros
+    W_in = lil_matrix(W_in_shape)
+    # set the seeds
+    rnd0 = np.random.RandomState(W_in_seeds[0])
+    rnd1 = np.random.RandomState(W_in_seeds[1])
+
+    # make W_in
+    n_cols = W_in_shape[1] - input_bias_len
+    for j in range(W_in_shape[0]):
+        rnd_idx = rnd0.randint(0, n_cols)  # low inclusive, high exclusive
+        # only one element different from zero
+        # sample from the uniform distribution
+        W_in[j, rnd_idx] = rnd1.uniform(-1, 1)
+
+    # input bias is fully connected to the reservoir states
+    if input_bias_len > 0:
+        W_in[:, W_in_shape[1] - input_bias_len:] = rnd1.uniform(
+            -1, 1, (W_in_shape[0], input_bias_len)
+        )
+
+    W_in = W_in.tocsr()
+
+    return W_in
 
 def sparse_grouped(W_in_shape, W_in_seeds):
     # The inputs are not connected but they are grouped within the matrix
@@ -53,6 +88,27 @@ def sparse_grouped(W_in_shape, W_in_seeds):
     W_in = W_in.tocsr()
     return W_in
 
+def sparse_grouped_dense_input_bias(W_in_shape, W_in_seeds, input_bias_len):
+    # The inputs are not connected but they are grouped within the matrix
+
+    # initialize W_in with zeros
+    W_in = lil_matrix(W_in_shape)
+    rnd0 = np.random.RandomState(W_in_seeds[0])
+
+    n_cols = W_in_shape[1] - input_bias_len
+    for i in range(W_in_shape[0]):
+        W_in[
+            i,
+            int(np.floor(i * n_cols / W_in_shape[0])),
+        ] = rnd0.uniform(-1, 1)
+
+    # input bias is fully connected to the reservoir states
+    if input_bias_len > 0:
+        W_in[:, W_in_shape[1] - input_bias_len:] = rnd0.uniform(
+            -1, 1, (W_in_shape[0], input_bias_len)
+        )
+    W_in = W_in.tocsr()
+    return W_in
 
 def dense(W_in_shape, W_in_seeds):
     # The inputs are all connected
--- jax_esn/generate_input_weights.py
+++ jax_esn/generate_input_weights.py
@@ -5,11 +5,11 @@ import jax.numpy as jnp
 
 
 def sparse_random(W_in_shape: tuple, W_in_seeds: list):
-    """
-    Create the input weights matrix. Inputs are not connected, except for the parameters.
+    """Create the input weights matrix. 
+    Inputs are not connected and the weights are randomly placed one per row.
 
     Args:
-        W_in_shape (tuple): (N_reservoir, N_inputs + N_input_bias + N_param_dim)
+        W_in_shape (tuple): (N_reservoir, N_inputs + N_input_bias)
         W_in_seeds (list): A list of seeds for the random generators; one for the column index, one for uniform sampling
 
     Returns:
@@ -29,6 +29,42 @@ def sparse_random(W_in_shape: tuple, W_in_seeds: list):
     # W_in = sparse.csr_fromdense(W_in)
     return W_in
 
+def sparse_random_dense_input_bias(W_in_shape: tuple, W_in_seeds: list, input_bias_len: int):
+    """Create the input weights matrix. 
+    Inputs are not connected and the weights are randomly placed one per row.
+    However, the input bias is densely connected to ALL reservoir state variables
+
+    Args:
+        W_in_shape: N_reservoir x (N_inputs + N_input_bias)
+        W_in_seed: Seed for the random generator
+
+    Returns:
+        W_in: Sparse matrix containing the input weights
+    """
+
+    W_in = jnp.zeros(W_in_shape)
+    key = jax.random.PRNGKey(W_in_seeds[0])
+    rnd0, rnd1 = jax.random.split(key, 2)
+
+    n_cols = W_in_shape[1] - input_bias_len
+    # only one element different from zero sample from the uniform distribution
+    rnd_idx = jax.random.randint(rnd0, shape=(W_in.shape[0],), minval=0, maxval=n_cols)
+    row_idx = jnp.arange(0, W_in.shape[0])
+    uniform_values = jax.random.uniform(
+        rnd1, shape=(W_in.shape[0],), minval=-1.0, maxval=1.0
+    )
+
+    # Set values in the matrix at the specified indices
+    W_in = W_in.at[row_idx, rnd_idx].set(uniform_values)
+
+    # input bias is fully connected to the reservoir states
+    if input_bias_len > 0:
+        rnd2, _ = jax.random.split(rnd1, 2)
+        uniform_values2 = jax.random.uniform(
+            rnd2, shape=(W_in.shape[0],), minval=-1.0, maxval=1.0
+        )
+        W_in = W_in.at[:, W_in.shape[1] - input_bias_len].set(uniform_values2)
+    return W_in
 
 def sparse_grouped(W_in_shape: tuple, W_in_seeds: list):
     """
@@ -55,6 +91,39 @@ def sparse_grouped(W_in_shape: tuple, W_in_seeds: list):
     # W_in = sparse.csr_fromdense(W_in)
     return W_in
 
+def grouped_sparse_dense_input_bias(W_in_shape: tuple, W_in_seeds: list, input_bias_len: int):
+    """
+    Create a grouped input weights matrix.
+    The inputs are not connected, but they are grouped within the matrix.
+
+    Args:
+        W_in_shape: (N_reservoir, N_inputs + N_input_bias)
+        W_in_seed: Seed for the random generator
+    """
+
+    W_in = jnp.zeros(W_in_shape)
+    key = jax.random.PRNGKey(W_in_seeds[0])
+    rnd0, _ = jax.random.split(key)
+
+    # Generate row and column indices
+    n_cols = W_in_shape[1] - input_bias_len
+    row_idx = jnp.arange(0, W_in.shape[0])
+    column_idx = jnp.floor(row_idx * n_cols / W_in_shape[0]).astype(int)
+    uniform_values = jax.random.uniform(
+        rnd0, shape=(W_in.shape[0],), minval=-1.0, maxval=1.0
+    )
+
+    # Set values in the matrix at the specified indices
+    W_in = W_in.at[row_idx, column_idx].set(uniform_values)
+
+    # input bias is fully connected to the reservoir states
+    if input_bias_len > 0:
+        rnd1, _ = jax.random.split(rnd0, 2)
+        uniform_values1 = jax.random.uniform(
+            rnd1, shape=(W_in.shape[0],), minval=-1.0, maxval=1.0
+        )
+        W_in = W_in.at[:, W_in.shape[1] - input_bias_len].set(uniform_values1)
+    return W_in
 
 def dense(W_in_shape: tuple, W_in_seeds: list):
     """
